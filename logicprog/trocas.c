a0b39bd00d393840dfe374373575e729dd777d1f
Murillo <tumusx1@gmail.com>
2021-11-27 20:44:23 -0300

    first commit

---
+++ logicprog/trocas.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+int main() {
+    float precoUnitarioProduto, DinheiroRecebido, Troco, precoTotal;
+    int qntdComprada;
+
+    printf("Insira o preco do produto: \n");
+    scanf("%f", &precoUnitarioProduto);
+
+
+    printf("Insira a quantidade que voce deseja comprar: ");
+    scanf("%d", &qntdComprada);
+
+    printf("\nInsira a quantidade de dinheiro que foi recebido: ");
+    scanf("%f", &DinheiroRecebido);
+
+    Troco = DinheiroRecebido - (precoUnitarioProduto * qntdComprada);
+    precoTotal = qntdComprada * precoUnitarioProduto;
+
+
+    if (DinheiroRecebido >= precoTotal) {
+        printf("\nTroco eh %f", Troco);
+    }
+
+     else {
+
+        printf("Voce estah devendo %f", Troco);
+
+    }
+    return 0;
+}
