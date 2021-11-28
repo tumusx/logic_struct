88137775739361f479e044f05d291cddcfe19488
Murillo <tumusx1@gmail.com>
2021-11-27 22:03:15 -0300

    implement 'medidas'

--- logicprog/medidas.c
+++ logicprog/medidas.c
@@ -2,19 +2,19 @@
 #include "math.h"
 
 int main() {
-    float a1, b1, c1, a2, b2, c2, a3, b3, a3, totalQuadrado, totalTriangulo, totalTrapezio;
+    float a, b, c, totalQuadrado, totalTriangulo, totalTrapezio;
 
     printf("Insira o valor de A lado do quadrado do quadrado: ");
-    scanf("%f", &a1);
-    totalQuadrado = a1 * a1;
+    scanf("%f", &a);
+    totalQuadrado = a * a;
 
     printf("Insira o valor de A e B para o triangulo: ");
-    scanf("%f %f", &a2, &b2);
-    totalTriangulo = (a2 * b2) / 2;
+    scanf("%f, %f", &a, &b);
+    totalTriangulo = (a * b) / 2;
 
     printf("Insira o valor de A, B e C para calcular o valor da area do trapezio: ");
-    scanf("%f %f %f", &a3, &b3, &c3);
-    totalTrapezio = ((a3 + b3) * c3) / 2;
+    scanf("%f, %f, %f", &a, &b, &c);
+    totalTrapezio = ((a + b) * c) / 2;
 
     printf("Area do quadrado: %f", totalQuadrado);
     printf("Area do triangulo: %f", totalTriangulo);
