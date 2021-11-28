af2f428715f90043f54be30042429415802beacc
Murillo <tumusx1@gmail.com>
2021-11-27 21:01:45 -0300

    pow -> pi

---
+++ logicprog/circulo.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "math.h"
+#define _USE_MATH_DEFINES
+
+int main(){
+    float raioCirculo, areaCirculo;
+
+    printf("Insira o raio do circulo: ");
+    scanf("%f", &raioCirculo);
+
+    areaCirculo = M_PI * pow(raioCirculo, 2);
+
+    printf("A area do cirulo eh: %f", areaCirculo );
+
+
+    return 0;
+}
+
