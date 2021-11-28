87f337f2c809ea9524eb6ec175b872eb47a4db4d
Murillo <tumusx1@gmail.com>
2021-11-27 21:29:27 -0300

    implement file about payment for collaborator

---
+++ logicprog/pagamento.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "conio.h"
+
+int main() {
+    float valorHora, aReceber;
+    int horasTrabalhadas;
+    char nomeColaborador[20];
+
+    printf("Insira o nome do colaborador : ");
+    scanf(" %s", nomeColaborador);
+
+    printf("Insira quantos recebe por horas: ");
+    scanf("%f", &valorHora);
+
+    printf("Insira a quantidade de horas trabalhada: ");
+    scanf("%d", &horasTrabalhadas);
+
+    aReceber = horasTrabalhadas * valorHora;
+
+    printf("O payment para o collaborator %s deve ser %0.2f", nomeColaborador, aReceber);
+
+    getch();
+
+    return 0;
+
+
+
+}
+
